cfd79e1d464afff72eef7426469ecbd81bc84929
H3rN4n <desouza030689@gmail.com>
2015-10-13 18:14:37 -0300

    including dependencies

--- modules/core/client/app/config.js
+++ modules/core/client/app/config.js
@@ -4,7 +4,15 @@
 var ApplicationConfiguration = (function () {
   // Init module configuration options
   var applicationModuleName = 'mean';
-  var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ngMessages', 'ui.router', 'ui.bootstrap', 'ui.utils', 'angularFileUpload', 'ngLodash'];
+  var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ngMessages', 'ui.router', 'ui.bootstrap', 'ui.utils', 'angularFileUpload', 'ngLodash','geolocation',
+    'restangular',
+    'uiGmapgoogle-maps',
+    'datePicker',
+    'ngStorage',
+    'textAngular',
+    'angularMoment',
+    'ui.checkbox',
+    'djds4rce.angular-socialshare',];
 
   // Add a new vertical module
   var registerModule = function (moduleName, dependencies) {
--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,95 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$http', 'geolocation', '$location',
+  function ($scope, Authentication, $http, geolocation, $location) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    $scope.founds = {};
+    $scope.dates = [];
+    $scope.adoption = [];
+    $scope.missing = [];
+    $scope.vets = [];
+
+    $scope.checkAuthentication = function(){
+      if($scope.authentication && $scope.authentication.user._id){
+        console.log($location);
+        var currentLocation = $location.path();
+        if(currentLocation !== '/'){
+          $location.path('/');
+        }
+      }
+    };
+
+    var getVets = function() {
+      geolocation
+          .getLocation()
+          .then(
+          function (data) {
+            $scope.radioSelected = '1000';
+            $http.get('/api/vets', {
+              params: {
+                latitude: data.coords.latitude,
+                longitude: data.coords.longitude,
+                radio: $scope.radioSelected
+              }
+            }).
+                success(function(data, status, headers, config) {
+                  $scope.vets = data.slice(0,4);
+                }).
+                error(function(data, status, headers, config) {
+                });
+
+          });
+
+
+    };
+
+    var getPetDates = function() {
+      $http.get('/api/pets/dates').
+          success(function(data, status, headers, config) {
+            $scope.dates = data;
+          }).
+          error(function(data, status, headers, config) {
+          });
+    };
+
+    var getPetAdoption = function(){
+      $http.get('/api/pets/adoption').
+          success(function(data, status, headers, config) {
+            $scope.adoption = data;
+          }).
+          error(function(data, status, headers, config) {
+          });
+    };
+
+    var getPetMissing = function(){
+      $http.get('/api/pets/missing').
+          success(function(data, status, headers, config) {
+            $scope.missing = data;
+          }).
+          error(function(data, status, headers, config) {
+          });
+    };
+
+    getPetMissing();
+
+    getPetDates();
+
+    getPetAdoption();
+
+    getVets();
+
+
+    $scope.founds.foundCounter = 1000.00;
+
+    $scope.increaseCounter = function(){
+      setInterval(function(){
+        $scope.founds.foundCounter = $scope.founds.foundCounter + Math.floor((Math.random() * 10) + 1);
+        $scope.$apply();
+      }, 1000);
+    };
+
+    $scope.increaseCounter();
   }
 ]);
